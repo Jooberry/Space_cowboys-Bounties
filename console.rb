7f2315787340ff6998cfb62a58b5cc4d13a64a78
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-21 15:57:37 +0000

    made save, update, delete, find methods

--- console.rb
+++ console.rb
@@ -1,6 +1,25 @@
 require('pry')
 require_relative('models/bounty')
 
+Bounty.delete_all()
+
+bounty1 = Bounty.new({
+  'name' => 'Joo',
+  'danger_level' => 'ermagerd',
+  'homeworld' => 'Orient World',
+  'favourite_weapon' => 'insults'
+  })
+bounty2 = Bounty.new({
+  'name' => 'Allegra',
+  'danger_level' => 'annoying',
+  'homeworld' => 'Teviot Place',
+  'favourite_weapon' => 'catchy songs'
+  })
+
+bounty1.save()
+bounty2.save()
+
+
 
 binding.pry
 nil
--- models/bounty.rb
+++ models/bounty.rb
@@ -2,6 +2,7 @@ require('pg')
 
 class Bounty
 
+  attr_reader :id
   attr_writer :name, :danger_level, :homeworld, :favourite_weapon
 
   def initialize(options)
@@ -12,10 +13,68 @@ class Bounty
     @favourite_weapon = options['favourite_weapon']
   end
 
-  # def save()
-  #   db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+  def save()
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "INSERT INTO space_cowboys (
+    name,
+    danger_level,
+    homeworld,
+    favourite_weapon
+    ) VALUES (
+    '#{@name}',
+    '#{@danger_level}',
+    '#{@homeworld}',
+    '#{@favourite_weapon}') RETURNING *"
+    @id = db.exec(sql).first()["id"].to_i
+    db.close()
   end
 
+  def delete()
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "DELETE FROM space_cowboys WHERE id = #{@id}"
+    db.exec(sql)
+    db.close
+  end
 
+  def update()
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "UPDATE space_cowboys 
+      SET (
+      name, 
+      danger_level, 
+      homeworld, 
+      favourite_weapon) = (
+      '#{@name}', 
+      ' #{@danger_level}', 
+      '#{@homeworld}', 
+      '#{@favourite_weapon}')
+      WHERE id = #{@id}"
+    db.exec(sql)
+    db.close()
+  end
+
+  def self.find(name)
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "SELECT * FROM space_cowboys WHERE name = '#{name}'"
+    list = db.exec(sql)
+    db.close
+    return list.map{|bounty|Bounty.new(bounty)}
+  end
+
+  def self.find_by_id(id)
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "SELECT * FROM space_cowboys WHERE id = #{id}"
+    list = db.exec(sql)
+    db.close
+    found = list.map{|bounty|Bounty.new(bounty)}
+    return found.first
+  end
+
+  def self.delete_all()
+    db = PG.connect({dbname: 'space_cowboys', host: 'localhost'})
+    sql = "DELETE FROM space_cowboys"
+    db.exec(sql)
+    db.close()
+  end
 
 end
